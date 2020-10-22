7179f4565dd0bcf1d4209d91d480ea39fce8c63d
Cyrus <cyrusbhandari09@gmail.com>
2020-10-22 18:22:01 +0530

    Name change for Basics, Basics2 created

---
+++ Basics2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+
+// Let's make a functoin that gives us the cube of a number
+// Then, we'll 'call' this function
+
+void Fn(string Name, double Num)
+{
+    cout << "Enter your name: ";
+    getline (cin, Name);
+    
+    cout << "Hello " << Name << ". Enter a number : " << endl;
+    cin >> Num;
+
+    cout << "The cube of " << Num << " is " << pow(Num, 3) << endl;
+
+
+}
+
+
+int main()
+{
+  Fn("X",1);
+ 
+  return 0;
+}
