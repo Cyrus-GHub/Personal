8b53c32dc3b5b355bd97460bd7526464dededcef
Cyrus <cyrusbhandari09@gmail.com>
2020-11-14 09:14:36 +0530

    grade

--- C programmes/QPaper1/!GradeSystem.c
+++ C programmes/QPaper1/!GradeSystem.c
@@ -8,27 +8,27 @@ int main()
     printf("Enter total marks secured: ");
     scanf("%d", &marks);
 
-    if (marks >= 91)
+    if (marks >= 91 && marks <= 100)
     {
         c = 1;
     }
 
-    else if (marks >= 81)
+    else if (marks >= 81 && marks < 91)
     {
         c = 2;
     }
 
-    else if (marks >= 71)
+    else if (marks >= 71 && marks < 81)
     {
         c = 3;
     }
 
-    else if (marks >= 61)
+    else if (marks >= 61 && marks < 71)
     {
         c = 4;
     }
 
-    else if (marks >= 51)
+    else if (marks >= 51 && marks < 61)
     {
         c = 5;
     }
@@ -38,33 +38,36 @@ int main()
         c = 6;
     }
 
-
-
     switch (c)
     {
     case 1:
         grade = 'O';
+        printf("%c", grade);
         break;
     case 2:
         grade = 'E';
+        printf("%c", grade);
         break;
     case 3:
         grade = 'A';
+        printf("%c", grade);
         break;
     case 4:
         grade = 'B';
+        printf("%c", grade);
         break;
     case 5:
         grade = 'C';
+        printf("%c", grade);
         break;
     case 6:
         grade = 'F';
+        printf("%c", grade);
         break;
     default:
         grade = 'X';
+        printf("%c", grade);
     }
-
-    printf("\n Your Grade is: %c ", grade);
 
     return 0;
 }
