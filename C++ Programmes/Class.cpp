55160f8f77eb979a9d43347d85bcd0d931a4a029
Cyrus <cyrusbhandari09@gmail.com>
2020-11-14 12:00:37 +0530

    class program

--- C++ Programmes/Class.cpp
+++ C++ Programmes/Class.cpp
@@ -17,10 +17,24 @@ int main()
             aDisplay = Display;
             aRAM = RAM;
         }
+
+        bool Enough_RAM()
+        {
+            if (RAM >= 6)
+            {
+                return true;
+            }
+
+            else
+                return false;
+        }
     };
 
     Phone P1("Pixel 5", "Snapdragon 765G", "6 inch 90Hz OLED", 8);
     Phone P2("Pixel 4a 5G", "Snapdragon 765G", "6.2 inch 60Hz OLED", 6);
+    Phone P3("Pixel 3a", "Snapdragon 670", "5.6 inch 60Hz OLED", 4);
+
+    cout << P3.Enough_RAM();
 
     return 0;
 }
