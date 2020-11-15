e012bad4dfc22b0ec8a5b238153a9ae8b067640c
Cyrus <cyrusbhandari09@gmail.com>
2020-11-15 21:10:17 +0530

    something

--- C programmes/QPaper1/!Calculator.c
+++ C programmes/QPaper1/!Calculator.c
@@ -3,13 +3,12 @@ int main()
 {
     int a, b;
     char op;
-
-    printf("Enter first number: ");
-    scanf("%d", &a);
-
     printf("\n List of available operators: +(add), -(subtract), *(multiply), /(divide)");
     printf("\n Enter Operator: ");
     scanf("%c", &op);
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
 
     printf("\n Enter second number: ");
     scanf("%d", &b);
--- C++ Programmes/!Class.cpp
+++ C++ Programmes/!Class.cpp
@@ -4,18 +4,40 @@ int main()
 {
     class Phone
     {
+    private:
+        string Year;
+
     public:
         string Model;
         string SOC;
         string Display;
         int RAM;
 
-        Phone(string aModel, string aSOC, string aDisplay, int aRAM)
+        Phone(string aModel, string aYear, string aSOC, string aDisplay, int aRAM)
         {
-            aModel = Model;
-            aSOC = SOC;
-            aDisplay = Display;
-            aRAM = RAM;
+            Model = aModel;
+            SOC = aSOC;
+            Display = aDisplay;
+            RAM = aRAM;
+            someYear(aYear);
+        }
+
+        string someYear(string aYear)
+        {
+            if (aYear == "2020" || aYear == "2019")
+                Year = aYear;
+
+            else
+            {
+                Year = "Old Device";
+            }
+
+            return 0;
+        }
+
+        string ModelYear()
+        {
+            return Year;
         }
 
         bool Enough_RAM()
@@ -30,11 +52,14 @@ int main()
         }
     };
 
-    Phone P1("Pixel 5", "Snapdragon 765G", "6 inch 90Hz OLED", 8);
-    Phone P2("Pixel 4a 5G", "Snapdragon 765G", "6.2 inch 60Hz OLED", 6);
-    Phone P3("Pixel 3a", "Snapdragon 670", "5.6 inch 60Hz OLED", 4);
+    Phone P1("Pixel 5", "2020", "Snapdragon 765G", "6 inch 90Hz OLED", 8);
+    Phone P2("Pixel 4a 5G", "2020", "Snapdragon 765G", "6.2 inch 60Hz OLED", 6);
+    Phone P3("Pixel 3a", "2019", "Snapdragon 670", "5.6 inch 60Hz OLED", 4);
+    Phone P4("LG G6", "2017", "Snapdragon 820", "5.8 inch 60Hz LCD", 4);
 
-    cout << P3.Enough_RAM();
+    cout << P3.Enough_RAM() << "\n";
+    cout << P3.ModelYear() << "\n";
+    cout << P4.ModelYear();
 
     return 0;
 }
