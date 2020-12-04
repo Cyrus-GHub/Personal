ca7bcaf4a5210d527f2d6bcc8f7fdacf93117b39
Cyrus <cyrusbhandari09@gmail.com>
2020-12-04 15:25:32 +0530

    unsigned int used in 11.c

--- C programmes/ANSI C/11.c
+++ C programmes/ANSI C/11.c
@@ -2,11 +2,11 @@
 #include <math.h>
 int main()
 {
-    int a, b, c;
+    unsigned int a, b, c;
     float s, area;
 
     printf("Enter the sides of triangle: ");
-    scanf("%d %d %d", &a, &b, &c);
+    scanf("%u %u %u", &a, &b, &c);
 
     s = (a + b + c) / 2;
     area = sqrt(s * (s - a) * (s - b) * (s - c));
